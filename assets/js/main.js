36630f6f10b50dc50861e46c4c7e32bb5397df50
Wuicho <130885313+wuiWuicho33@users.noreply.github.com>
2025-03-31 14:01:37 -0600

    Se separa el script del modal

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,40 +1,6 @@
 /*=========
 COMPONENTES
 =========*/
-/* # Modal
----------------------------------------------- */
-document.querySelectorAll('[data-modal]').forEach(button => {
-  button.addEventListener('click', function() {
-      const modalId = this.getAttribute('data-modal');
-      openModal(modalId);
-  });
-});
-
-document.querySelectorAll('[data-dismiss="modal"]').forEach(button => {
-  button.addEventListener('click', function() {
-      const modal = this.closest('.modal');
-      closeModal(modal.id);
-  });
-});
-
-function openModal(modalId) {
-  const modal = document.getElementById(modalId);
-  modal.style.display = "flex"; // Usamos "flex" para mostrar el modal centrado
-}
-
-function closeModal(modalId) {
-  const modal = document.getElementById(modalId);
-  modal.style.display = "none"; // Usamos "none" para ocultar el modal
-}
-
-// Cerrar el modal al hacer clic fuera de él
-window.onclick = function(event) {
-  if (event.target.classList.contains('modal')) {
-      closeModal(event.target.id);
-  }
-};
-
-
 /* # Switches
 ---------------------------------------------- */
 // Función para actualizar el texto del label basado en el estado del switch
